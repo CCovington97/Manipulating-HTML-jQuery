2245ba2f27f1de26c08681bd5556d5572f6637ed
Connor <ccovington439@gmail.com>
2020-01-28 16:53:16 -0600

    jQuery step 4 complete

--- jQuery/jQueryScript.js
+++ jQuery/jQueryScript.js
@@ -22,4 +22,14 @@ $(document).ready(function () {
     $(div3).mouseleave(function() {
         $(div3).css('background-color', 'black')
     })
+
+    let p = $('<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque nostrum amet, eveniet repellat ea id enim quae. Perferendis a necessitatibus quas omnis! Eius eveniet, nobis itaque repudiandae provident minima totam!</p>');
+    $(div).append(p)
+    let colors = ['blue', 'red', 'green', 'purple', 'yellow', 'orange', 'cyan', 'pink', 'chartreuse', 'violet', 'gold', 'coral' ]
+
+    p.click(function () {
+        $(p).css('color', colors[Math.floor(Math.random() * colors.length)])
+    })
+
+    
 })
